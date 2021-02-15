4a34375c5743cf04c850264ccd5a6a1ec7f53434
Matt Lowe <matlow100@yahoo.com>
2021-02-15 17:20:25 -0600

    Game checks if a piece is in the correct slot

---
+++ Assets/Scripts/PuzzleChecker.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleChecker : MonoBehaviour
+{
+    public Piece piece;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Piece"))
+        {
+            if (GameObject.ReferenceEquals(other.gameObject, piece.gameObject))
+            {
+                Debug.Log("You did it.");
+            }
+        }
+    }
+}
