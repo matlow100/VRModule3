e837138bb426fd20a1aabb163c9d9c4af1801328
Matt Lowe <matlow100@yahoo.com>
2021-02-24 16:42:38 -0600

    Fixed slot-piece matchup

--- Assets/Scripts/CountManager.cs
+++ Assets/Scripts/CountManager.cs
@@ -7,6 +7,13 @@ public class CountManager : MonoBehaviour
 {
     private int count;
     public Text winText;
+    public GameObject winningMap;
+    public GameObject piece1;
+    public GameObject piece2;
+    public GameObject piece3;
+    public GameObject piece4;
+    public GameObject piece5;
+    public GameObject piece6;
 
     void Start()
     {
@@ -21,6 +28,13 @@ public class CountManager : MonoBehaviour
         {
             Debug.Log("You Win!");
             winText.gameObject.SetActive(true);
+            winningMap.gameObject.SetActive(true);
+            piece1.gameObject.SetActive(false);
+            piece2.gameObject.SetActive(false);
+            piece3.gameObject.SetActive(false);
+            piece4.gameObject.SetActive(false);
+            piece5.gameObject.SetActive(false);
+            piece6.gameObject.SetActive(false);
         }
     }
 
